cf6fc752541813a4b96f469e7db567682419f976
Ido Rosenberg <ido.rosenberg@post.runi.ac.il>
2024-01-22 22:23:30 +0200

    'Check'

---
+++ TestEnvironment.java
@@ -0,0 +1,13 @@
+class TestEnvironment {
+    public static void main(String args[]) {
+        
+	int i=0;
+	while(i<4){
+	System.out.println("nir is gay");
+	i++;
+}
+	System.out.println("done");
+
+    }
+
+}
